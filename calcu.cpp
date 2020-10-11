914d73238e7ac70699b2dd6200a8f624d97039bf
Dave Morata <moratadave@yahoo.com>
2020-10-12 02:31:11 +0800

    calcu.cpp

---
+++ calcu.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+.git/char eq;
+.git/int fn,sn;
+.git/
+.git/cout <<First number, operator , Second number: ;
+.git/cin >>fn>>eq>>sn;
+.git/
+.git/switch(eq)
+.git/{
+.git/case '*':
+.git/cout <<=  << fn * sn;
+.git/break;
+.git/
+.git/case '/':
+.git/cout <<=  << fn / sn;
+.git/break;
+.git/
+.git/case '+':
+.git/cout <<=  << fn + sn;
+.git/break;
+.git/
+.git/case '-':
+.git/cout <<=  << fn - sn;
+.git/break;
+.git/
+.git/default:
+.git/cout <<Syntax Error!;
+.git/break;
+.git/}
+}
